4a054b3bdda0c7d63856e58c53d90df20e4d2cd7
MEGHARSHA A GOWDA <meghaharsha72@gmail.com>
2024-11-20 11:37:10 +0530

    final landing

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -3,8 +3,8 @@ import arrow from '../assets/images/arrow-right.svg';
 
 const classes = {
   container: 'flex flex-col h-screen items-center bg-black text-white width-[90%]',
-  h1: 'text-[40px] mt-[180px]',
-  h2: 'text-[150px] font-semibold',
+  h1: 'text-[50px] mt-[180px]',
+  h2: 'text-[140px] font-semibold',
   h3: 'text-[23px] mt-[20px] text-zinc-400',
   button: 'text-[18px] text-semibold mt-[30px] bg-[#141414] border-solid border-[2px] border-zinc-700 hover:border-black px-[50px] py-[10px] rounded-[30px] hover:bg-[#006cff] cursor-pointer',
   img: 'w-[20px] h-[20px] ml-[10px] inline mb-[3px]'
