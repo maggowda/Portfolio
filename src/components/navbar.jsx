6f7469a46464682b6cc0789460008ba06b8c3e34
MEGHARSHA A GOWDA <meghaharsha72@gmail.com>
2024-11-20 17:26:21 +0530

    new addition

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import mag from '../assets/images/mag.svg';
 
 const classes = {
-    container: 'font-mono fixed flex mt-[20px] mx-auto px-4 rounded-[30px] bg-white/3 backdrop-blur-md h-[60px] w-[90%] justify-between items-center',
+    container: 'font-mono fixed flex mt-[20px] mx-auto px-4 rounded-[30px] bg-white/3 backdrop-blur-md h-[60px] w-[90%] justify-between items-center z-[1000]',
     container1: 'flex justify-around items-center',
     item: 'cursor-pointer text-white px-4 pb-[6px] pt-[5px] hover:bg-white/20 hover:backdrop-blur-sm hover:text-white rounded-[10px]',
     img: 'h-[60px] w-[150px] cursor-pointer mix-blend-multiply',
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import arrow from '../assets/images/arrow-right.svg';
 
 const classes = {
-  container: 'flex flex-col items-center bg-black text-white width-[90%] mb-[50px]',
+  container: 'flex flex-col items-center bg-black text-white width-[90%]',
+  container1: 'flex flex-row justify-around items-center w-full mt-[-80px]',
+  container2: 'flex flex-col',
+  container3: 'flex flex-col mt-[200px]',
   h1: 'text-[50px] mt-[180px]',
   h2: 'text-[140px] font-semibold',
-  h3: 'text-[23px] mt-[15px] text-zinc-400',
-  button: 'text-[18px] text-semibold mt-[30px] bg-[#141414] border-solid border-[2px] border-zinc-700 hover:border-black px-[50px] py-[10px] rounded-[30px] hover:bg-[#006cff] cursor-pointer',
-  img: 'w-[20px] h-[20px] ml-[10px] inline mb-[3px]'
+  h3: 'text-[23px] mt-[15px] mb-[25px] text-zinc-400',
+  h4: 'text-[55px] mt-[70px] mb-[20px] text-center',
+  button: 'text-[18px] text-semibold bg-[#141414] border-solid border-[2px] border-zinc-700 hover:border-black px-[50px] py-[10px] rounded-[30px] hover:bg-[#006cff] cursor-pointer',
+  img: 'w-[20px] h-[20px] ml-[10px] inline mb-[3px]',
+  div0: 'w-[1100px] h-[650px] mt-[95px] bg-zinc-700 rounded-[50px] z-30',
+  div1: 'w-[1100px] h-[650px] mt-[50px] bg-zinc-700 rounded-[50px] z-20',
+  div2: 'w-[1100px] h-[650px] my-[50px] bg-zinc-700 rounded-[50px] z-10',
+  div3: 'w-[510px] h-[300px] rounded-[30px] bg-zinc-700 mb-[40px]' 
 }
 function Landing() {
   return (
@@ -16,6 +24,23 @@ function Landing() {
       <span className={classes.h2} id='font'>Megharsha</span>
       <div className={classes.h3}>Freelance UX UI Designer and Web Developer.<br/></div>
       <div className={classes.button}>Start a project<img src={arrow} className={classes.img}/></div>{/*add smooth to button*/}
+      <div className={classes.div0}></div>
+      <div className={classes.div1}></div>
+      <div className={classes.div2}></div>
+      <div className={classes.h4}>High quality web design <br/>and development</div>
+      <div className={classes.container1}>
+        <div className={classes.container2}>
+          <div className={classes.div3}></div>
+          <div className={classes.div3}></div>
+        </div>
+        <div className={classes.container3}>
+          <div className={classes.div3}></div>
+          <div className={classes.div3}></div>
+        </div>
+      </div>
+      <div className={classes.button}>View all projects<img src={arrow} className={classes.img}/></div>
+      <div></div>
+      <div></div>
     </div>
   )
 }
