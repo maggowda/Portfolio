dedaadb4474b26bd625426eb8d62b1fdfb01e633
MEGHARSHA A GOWDA <meghaharsha72@gmail.com>
2024-11-22 13:11:33 +0530

    done bro

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,11 +8,13 @@ import mail from '../assets/images/mail.svg';
 import location from '../assets/images/gps.svg';
 
 const classes = {
-    container: 'flex flex-row justify-between bg-neutral-900 text-white px-[45px] pt-[30px] pb-[45px] mb-[50px] rounded-[20px] mx-[110px]',
+    container0: 'flex flex w-full justify-center items-center',
+    container: 'flex flex-row justify-between bg-neutral-900 text-white px-[45px] pt-[30px] pb-[45px] mb-[50px] rounded-[20px]',
     img: 'w-[150px] h-[90px] ml-[-25px] mb-[-20px] mt-[-10px]',
     img2: 'w-[23px] h-[23px] inline',
     img3: 'w-[22px] h-[21px] inline',
     img4: 'w-[28px] h-[28px] inline',
+    container4: 'mr-[279px]',
     container1: 'flex flex-row',
     container2: 'flex flex-col mr-[60px]',
     container3: 'flex flex-col mr-[10px]',
@@ -23,23 +25,25 @@ const classes = {
 }
 function Footer() {
   return (
-    <div className={classes.container}>
-        <div>
-            <img src={mag} className={classes.img}/>
-            <div className={classes.copy}>&#169; Megharsha A Gowda<br/>All rights reserved </div>
-        </div>
-        <div className={classes.container1}>
-            <div className={classes.container2}>
-                <h1 className={classes.h1}>Social Networking</h1>
-                <a href='' className={classes.anc}>Instagram<img src={instagram} className={classes.img2}/></a>
-                <a href='' className={classes.anc}>Linkedin<img src={linkedin} className={classes.img2}/></a>
-                <a href='' className={classes.anc2}>Github<img src={github} className={classes.img3}/></a>
+    <div className={classes.container0}>
+        <div className={classes.container}>
+            <div className={classes.container4}>
+                <img src={mag} className={classes.img}/>
+                <div className={classes.copy}>&#169; Megharsha A Gowda<br/>All rights reserved </div>
             </div>
-            <div className={classes.container3}>
-                <h1 className={classes.h1}>Contact Me</h1>
-                <a href='' className={classes.anc}>Whatsapp<img src={whatsapp} className={classes.img3}/></a>
-                <a href='' className={classes.anc}>Email<img src={mail} className={classes.img4}/></a>
-                <a href='' className={classes.anc2}>Location<img src={location} className={classes.img4}/></a>
+            <div className={classes.container1}>
+                <div className={classes.container2}>
+                    <h1 className={classes.h1}>Social Networking</h1>
+                    <a href='' className={classes.anc}>Instagram<img src={instagram} className={classes.img2}/></a>
+                    <a href='' className={classes.anc}>Linkedin<img src={linkedin} className={classes.img2}/></a>
+                    <a href='' className={classes.anc2}>Github<img src={github} className={classes.img3}/></a>
+                </div>
+                <div className={classes.container3}>
+                    <h1 className={classes.h1}>Contact Me</h1>
+                    <a href='' className={classes.anc}>Whatsapp<img src={whatsapp} className={classes.img3}/></a>
+                    <a href='' className={classes.anc}>Email<img src={mail} className={classes.img4}/></a>
+                    <a href='' className={classes.anc2}>Location<img src={location} className={classes.img4}/></a>
+                </div>
             </div>
         </div>
     </div>
